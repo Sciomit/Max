463fa9bbd119f6588c8f5dcba142a2e6e88102e1
Sciomit <max.r@bronxsoftware.org>
2016-05-13 13:31:15 -0400

    Create lab3.js

---
+++ lab3.js
@@ -0,0 +1,41 @@
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    
+   var ctx = canvas.getContext('2d');
+    //declare x and y as variables here
+    //assign each a value of 0
+  
+    
+    
+   //call the drawChar(x,y) function here
+
+  }
+}
+
+    
+    
+    
+    //add a function header here. the 
+    //function should be named drawChar(x,y)
+    var canvas = document.getElementById('canvas');
+      if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    
+        
+    //add a +x to each x coordinate
+    //add a +y to each y coordinate
+    ctx.fillStyle="purple";
+    ctx.fillRect(25,25,100,100);
+    
+    ctx.fillStyle="red";
+    ctx.fillRect(30, 30, 10, 10);
+    ctx.fillRect(60, 30, 10, 10);
+    
+    ctx.fillStyle="blue";
+    ctx.beginPath();
+    ctx.moveTo(40, 40);
+    ctx.lineTo(60, 40);
+    ctx.lineTo(50, 60);
+    ctx.fill();
+  }
